3712037f66584a5b6424dd103b9a55c68bceeeb0
Godslove Lee <godslovelee@yahoo.com>
2023-08-28 22:04:59 +0200

    import "dart convert"

--- lib/model/review_payload.dart
+++ lib/model/review_payload.dart
@@ -1,9 +1,13 @@
+import 'dart:convert';
+
+
 class HouseModel{
   final String curtains;
   final String windows;
   final String? floor;
 
   HouseModel(this.curtains, this.windows, this.floor);
+
 
 
 
